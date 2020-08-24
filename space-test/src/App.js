e66e17d28c38853426cadb20fc84e7896f78e8cf
Joanne Young <joanneyoung_@hotmail.com>
2020-08-24 11:23:45 +0100

    insert image slider

---
+++ space-test/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { Header } from './components/Header.component';
+import './App.css';
+
+export const App = () => {
+  
+  const [category, setCategory] = useState("dragons")
+  const [info, setInfo] = useState([])
+
+  useEffect(()=>{
+    fetch(
+      `https://api.spacexdata.com/v3/${category}`
+    )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setInfo(data)
+    })
+  })
+ 
+  return (
+    <div className="App container">
+    <Header />
+    <select id="drop-down" onChange={e => setCategory(e.target.value)}>
+      <option value="dragons">dragons</option>
+      <option value="rockets">rockets</option>
+    </select>
+    <p>The category is {category}</p>
+      <div className="cards">
+        {info.map((item, index) => (
+          <div id={item.id} className="card w-50 container">
+            <h1>{category === "rockets" ? item.rocket_name : item.name}</h1>
+            <p>{item.description}</p>
+            {item.flickr_images.map((url, index) => (
+              <img id={index} src={url} className="w-100"></img>
+            ))}
+          </div>
+        ))},
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ space-test/src/components/Header.component.js
@@ -0,0 +1,5 @@
+export const Header = (props) => {
+  return (
+    "Hello World!"
+  );
+}

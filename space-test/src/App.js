f3b870fa204057877d2ba06306eaa9d64f1c7cea
Joanne Young <joanneyoung_@hotmail.com>
2020-08-25 14:07:21 +0100

    Merged components into one page

--- space-test/src/App.js
+++ space-test/src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Header } from './components/Header.component';
-import { Selector } from './components/Selector.component';
 import './App.css';
 
 export const App = () => {
@@ -23,22 +21,51 @@ export const App = () => {
   return (
     <div className="App container">
     <Header />
-    <Selector category={category} setCategory={setCategory}/>
+    <Selector 
+      category={category} 
+      setCategory={setCategory}
+    />
     <p>The category is {category}</p>
-      <div className="cards">
-        {info.map((item, index) => (
-          <div id={item.id} className="card w-50 container">
-            <h1>{category === "rockets" ? item.rocket_name : item.name}</h1>
-            <p>{item.description}</p>
-            {item.flickr_images.map((url, index) => (
-              <img id={index} src={url} className="w-100"></img>
-            ))}
-          </div>
-        ))},
-      </div>
-
+    <CardSection 
+      info={info} 
+      category={category} 
+    />
     </div>
   );
 }
+
+export const Header = (props) => {
+  return (
+    "Hello World!"
+  );
+}
+
+export const Selector = (props) => {
+  return (
+    <select id="drop-down" onChange={e => props.setCategory(e.target.value)}>
+      <option value="dragons">dragons</option>
+      <option value="rockets">rockets</option>
+    </select>
+  ); 
+}
+
+export const CardSection = (props) => {
+
+  return (
+    <div className="cards">
+      {props.info.map((item, index) => (
+        <div id={item.id} className="card w-50 container">
+          <h1>{props.category === "rockets" ? item.rocket_name : item.name}</h1>
+          <p>{item.description}</p>
+          {item.flickr_images.map((url, index) => (
+            <img id={index} src={url} className="w-100"></img>
+          ))}
+        </div>
+      ))},
+    </div>
+  );
+}
+
+
 
 export default App;
--- space-test/src/components/Header.component.js
+++
@@ -1,5 +0,0 @@
-export const Header = (props) => {
-  return (
-    "Hello World!"
-  );
-}
